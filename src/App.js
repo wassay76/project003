c39ee728a2490799a1e1eefed4e7df5f55902aca
wassay76 <ab.wassey.76@gmail.com>
2020-11-25 17:45:01 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import hello from "./hello";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-    <br/>
-    hi hello 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      hello world
+      <br/>
+      <hello></hello>
     </div>
   );
 }
---
+++ src/hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function hello() {
+  return (
+      <p>this para graaph is from hello world</p>
+
+  );
+}
+
+export default hello;
